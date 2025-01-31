32191a13c68780d07e082477374fe2bb567651ac
Pansarekomal039 <pansarekomal039@gmail.com>
2025-01-31 19:03:28 +0530

    Program to convert celcius to farenheit

---
+++ Farenheit.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float c,f;
+cout << "Enter the temperature in Celcius" << endl;
+cin >> c;
+f=c*(9/5.0)+32;
+cout << "Fahrenheit = " << f << endl;
+return 0;
+}
+
